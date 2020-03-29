ea3c6721a7b2a563744067b68797dde9e00eeda0
Hope <aurelballin@ok.de>
2020-03-29 19:33:39 +0200

    adding easier Update Method

--- src/main/java/me/hope/franxxmin/listeners/CommandReprocessor.java
+++ src/main/java/me/hope/franxxmin/listeners/CommandReprocessor.java
@@ -381,7 +381,7 @@ public class CommandReprocessor {
                             event.getChannel().sendMessage(eb);
                             Runtime r = Runtime.getRuntime();
                             try {
-                                r.exec("screen -S botupdater -X stuff botupdate");
+                                r.exec("screen -S gitupdater -X stuff botupdate");
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
