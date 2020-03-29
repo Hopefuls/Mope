c496c7506fedc6c4113216d975c40149f5029f5d
Hope <aurelballin@ok.de>
2020-03-29 20:06:06 +0200

    everything

--- src/main/java/me/hope/franxxmin/listeners/CommandReprocessor.java
+++ src/main/java/me/hope/franxxmin/listeners/CommandReprocessor.java
@@ -382,7 +382,7 @@ public class CommandReprocessor {
                             Runtime r = Runtime.getRuntime();
 
                             try {
-                                r.exec("screen -S gitupdater -p 0 -X stuff 'botupdate'$(echo -ne '\\015')");
+                                r.exec("screen -S gitupdater -X stuff 'botupdate'$(echo -ne '\\015')");
                             } catch (IOException e) {
                                 e.printStackTrace();
                             }
